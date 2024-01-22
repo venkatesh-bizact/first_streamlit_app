d8ee3ccdf9670fbd293edb4433f54291f47b2956
Venkatesh Ananthoju <124863824+venkatesh-bizact@users.noreply.github.com>
2024-01-22 11:56:47 +0530

    Update streamlit_app.py
    testing streamlit functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.write('Hello')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlt.text('Hard-Boiled Free-Range Egg')
