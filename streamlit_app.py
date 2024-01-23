5919eb2112722e03b33c390f3082abd68c365555
Venkatesh Ananthoju <124863824+venkatesh-bizact@users.noreply.github.com>
2024-01-23 13:09:26 +0530

    Update streamlit_app.py
    fetching fruityvise json response

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,7 @@ streamlit.dataframe(fruits_to_show)
 
 # # display the table on the page
 # streamlit.dataframe(fruits_to_show)
+streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
