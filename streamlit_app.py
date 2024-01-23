3d2db8ef4bf5584f8a26fbd55edb7be59e2e05ee
Venkatesh Ananthoju <124863824+venkatesh-bizact@users.noreply.github.com>
2024-01-23 14:23:03 +0530

    Update streamlit_app.py
    added user interface to search for item

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,8 +29,10 @@ streamlit.dataframe(fruits_to_show)
 # # display the table on the page
 # streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 # take json version of responcer and normalize it
