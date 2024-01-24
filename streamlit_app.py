3e26127db8cde4207311bab15851c6517651c2ca
Venkatesh Ananthoju <124863824+venkatesh-bizact@users.noreply.github.com>
2024-01-24 12:00:59 +0530

    Update streamlit_app.py
    added function to make api call and avoid repetitive calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,17 +28,21 @@ streamlit.dataframe(fruits_to_show)
 # fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 # # fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# # display the table on the page
-# streamlit.dataframe(fruits_to_show)
+# create a repeatable code block called functon
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+# New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_functon = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_functon)
 except URLError as e:
   streamlit.error()
     
