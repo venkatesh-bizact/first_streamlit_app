2ca5759d23156bb8d74b77b9cd10cdc14dd969f0
Venkatesh Ananthoju <124863824+venkatesh-bizact@users.noreply.github.com>
2024-01-24 11:33:36 +0530

    Update streamlit_app.py
    added conditional & organized code

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,17 +31,18 @@ streamlit.dataframe(fruits_to_show)
 # # display the table on the page
 # streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+    
 streamlit.write('The user entered', fruit_choice)
-
-# import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-
-# take json version of responcer and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
